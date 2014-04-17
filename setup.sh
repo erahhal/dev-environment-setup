61544344b15ac0bbb05ee06350df9ec3f0473061
Ellis Rahhal <ellis.rahhal@spacex.com>
2014-04-17 16:24:39 -0700

    now using symbolic links to git repo; added bash and util scripts

--- setup.sh
+++ setup.sh
@@ -2,8 +2,11 @@
                                
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+ln -s $DIR/Scripts ~/Scripts
+ln -s $DIR/bash/bashrc ~/.bashrc
+ln -s $DIR/bash/bash_profile ~/.bash_profile
+ln -s $DIR/vim/vimrc ~/.vimrc     
 mkdir -p ~/.vim                
-cp $DIR/vim/vimrc ~/.vimrc     
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim -c "execute 'BundleInstall' | qa"
 cp -R ~/.vim/bundle/vim-colors-solarized/colors ~/.vim
