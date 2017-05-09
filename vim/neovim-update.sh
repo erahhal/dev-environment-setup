bc5a77fb08d30b2592eafc8b8e84037252066120
Ellis Rahhal <ellis.rahhal@spacex.com>
2017-05-09 13:29:37 -0700

    added neovim update script; updated ctrl-p settings to support large projects

---
+++ vim/neovim-update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$(uname)" == "Darwin" ]; then
+  brew update
+  brew upgrade neovim
+  gem install neovim
+  pip install --upgrade neovim
+else
+  sudo apt update
+  sudo apt upgrade -y neovim
+  sudo gem install neovim
+  sudo pip2 install --upgrade neovim
+  sudo pip3 install --upgrade neovim
+fi
+
+vim -c "execute 'PlugUpgrade' | qa"
+vim -c "execute 'PlugUpdate' | qa"
+vim -c "execute 'PlugInstall' | qa"
+vim -c "execute 'PlugClean' | qa"
+vim -c "execute 'UpdateRemotePlugins' | qa"
+
